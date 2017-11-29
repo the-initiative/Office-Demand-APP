117894a60efbcd4ee9bcfa300bc99daa00cf4f41
Szekely <albert.szekely@bertelsmann.de>
2017-11-29 17:53:39 +0200

    fireangular2 added

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -5,13 +5,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../../configs/environment';
+
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AboutModule,
+    HomeModule,
+    SharedModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
---
+++ src/configs/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC95vjlpn3TQbXhRyqh3Wp1pcHq3VvyOU8',
+    authDomain: 'office-demand-app.firebaseapp.com',
+    databaseURL: 'https://office-demand-app.firebaseio.com',
+    projectId: 'office-demand-app',
+    storageBucket: 'office-demand-app.appspot.com',
+    messagingSenderId: '557073778635'
+  }
+};
