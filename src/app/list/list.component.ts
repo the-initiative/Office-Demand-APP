1488bc254639ef6a1792e42742d835fffcf46b18
Szekely <albert.szekely@bertelsmann.de>
2017-12-11 17:02:23 +0200

    add/list demo

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+export interface Item { name: string; }
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  export class ListComponent {
+    private itemsCollection: AngularFirestoreCollection<Item>;
+    items: Observable<Item[]>;
+    constructor(private afs: AngularFirestore) {
+      this.itemsCollection = afs.collection<Item>('items');
+      this.items = this.itemsCollection.valueChanges();
+    }
+    addItem(item: Item) {
+      this.itemsCollection.add(item);
+    }
   }
-
-}
